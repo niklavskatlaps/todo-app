e0f8947c202a7da805c719e06609401851814851
Niklavs Katlaps <niklavs.katlaps@kevin.eu>
2023-07-02 23:29:00 +0300

    Minor styling adjustments

--- frontend/src/components/AddTodo/AddTodo.component.tsx
+++ frontend/src/components/AddTodo/AddTodo.component.tsx
@@ -10,9 +10,9 @@ export default function AddTodoComponent(props: Props) {
   const { todoTitle, onFormSubmit, onTodoTitleChange } = props;
 
   return (
-    <form className={ "row g-3 m-0" } onSubmit={ onFormSubmit }>
-      <input className={ "form-control" } type="text" placeholder="Enter a new todo item" value={ todoTitle } onChange={ onTodoTitleChange } />
-      <button className={ "btn btn-primary" } type="submit" disabled={ !todoTitle }>Add item</button>
+    <form className={ "row g-2 g-3 m-0" } onSubmit={ onFormSubmit }>
+      <input className={ "form-control col-md col-lg" } type="text" placeholder="Enter a new todo item" value={ todoTitle } onChange={ onTodoTitleChange } />
+      <button className={ "btn btn-primary col-md-3 col-lg-3 ms-md-2 ms-lg-2" } type="submit" disabled={ !todoTitle }>Add item</button>
     </form>
   );
 }
